19e4e1f1b22d35df74a8e0f53d9997116535ba49
Brice Decaestecker <brice.decaestecker@gmx.fr>
2024-11-28 06:14:30 +0100

    Setup test case for version convergence

---
+++ src/it/redundant/verify.groovy
@@ -0,0 +1,4 @@
+String buildLog = new File(basedir, "build.log").text
+assert buildLog.contains("[ERROR]")
+assert buildLog.contains("httpcore5-h2")
+assert buildLog.contains("it would not clash with any other dependency")
